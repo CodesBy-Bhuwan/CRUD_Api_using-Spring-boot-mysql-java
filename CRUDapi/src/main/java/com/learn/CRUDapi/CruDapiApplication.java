b36178160f7dad572030ebf0282448afba0e46ed
Bhuwna Thadarai <bhuwan.534610817loveswtkrokay@gmail.com>
2024-01-08 23:30:56 +0545

    Database config

---
+++ CRUDapi/src/main/java/com/learn/CRUDapi/CruDapiApplication.java
@@ -0,0 +1,13 @@
+package com.learn.CRUDapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CruDapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CruDapiApplication.class, args);
+	}
+
+}
---
+++ CRUDapi/src/test/java/com/learn/CRUDapi/CruDapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn.CRUDapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CruDapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
