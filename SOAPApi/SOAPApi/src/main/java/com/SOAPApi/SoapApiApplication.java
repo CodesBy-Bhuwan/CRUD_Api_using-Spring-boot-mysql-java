dd7bc7af06105ee9955ca22888b1f42792d3f026
CodesBy-Bhuwan <bhuwan.thadarai817@gmail.com>
2024-12-30 21:34:32 +0545

    SOAP web-services

---
+++ SOAPApi/SOAPApi/src/main/java/com/SOAPApi/SoapApiApplication.java
@@ -0,0 +1,13 @@
+package com.SOAPApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoapApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoapApiApplication.class, args);
+	}
+
+}
